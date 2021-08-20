bb854c5b430cb4115ac34ae60cf3b91823b53d91
SergeyOnline <SergeyOnline@protonmail.com>
2021-08-20 04:11:54 +0300

    Create LoginVC and ListVC

---
+++ SwiftyProtein/SwiftyProtein/LoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewController.swift
+//  SwiftyProtein
+//
+//  Created by Сергей on 19.08.2021.
+//
+
+import UIKit
+import LocalAuthentication
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+	@IBAction func useBiometrics(_ sender: UIButton) {
+		let context = LAContext()
+		
+		if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+			context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Please authenticate to proceed.") { success, error in
+				if success {
+					DispatchQueue.main.async {
+						//TODO: do something
+						self.dismiss(animated: true, completion: nil)
+					}
+				} else {
+					guard let error = error else { return }
+					print(error.localizedDescription)
+				}
+			}
+		}
+	}
+	
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
